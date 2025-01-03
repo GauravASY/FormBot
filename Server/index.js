e7efa2cef9d7e8ea0e7f92ee49aedccff477c747
GauravASY <gauravyadav199808@gmail.com>
2025-01-03 22:25:33 +0530

    Cors Updated

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: process.env.FRONTEND_URL,  
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        credentials: true,   
+      }
+));
 app.use(express.json());
 
 app.get('/', (req, res)=>{
